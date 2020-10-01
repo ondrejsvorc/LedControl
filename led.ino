e61763c6efd41f6098f47cb6933e148d8f86a9f3
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2020-10-01 19:22:23 +0200

    Arduino code

---
+++ led.ino
@@ -0,0 +1,25 @@
+const int led = 9;
+char state;
+
+void setup() {
+  
+    pinMode(led, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+
+  while (Serial.available() > 0)
+    {
+        state = Serial.read();
+   
+        if (state == '1')
+        {
+            digitalWrite(led, HIGH);
+        }
+        else
+        {   
+            digitalWrite(led, LOW);
+        }
+    }
+}
